ee04b2d559925011ca0d5cdaa0f35567f2938cef
Matt Winkler <matt@mattwinkler.com>
2016-11-11 23:37:23 -0800

    Ajax spinner and animate elements in/out

--- src/app/main/main-search.controller.js
+++ src/app/main/main-search.controller.js
@@ -9,8 +9,10 @@ export class MainSearchController {
   }
 
   activate() {
+    this.pending = true;
     this.Conversation.findAll({platform_id: 1}).then( (conversations) => {
       this.conversations = conversations;
+      this.pending = false;
       return conversations;
     });
   }
